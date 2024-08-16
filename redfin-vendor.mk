1582681937e9d94d4d19668a91ff929c34e01d56
Aaron Rieder <737456+AARtwo@users.noreply.github.com>
2024-08-16 09:46:25 -0400

    Update redfin-vendor.mk

--- redfin-vendor.mk
+++ redfin-vendor.mk
@@ -111,7 +111,6 @@ PRODUCT_PACKAGES += \
     EuiccGoogleOverlay \
     EuiccSupportPixel \
     EuiccSupportPixelPermissions \
-    HbmSVManager \
     MyVerizonServices \
     OBDM_Permissions \
     RilConfigService \
